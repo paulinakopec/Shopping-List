8ac83b68e1f30cd73bed6bd54ef1bd24b59d830f
paulinakopec <100046393+paulinakopec@users.noreply.github.com>
2022-02-19 21:32:07 +0100

    Update main.py
    Added counter and clear whole list.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import time
 lista_zakupow=[]
 while True:
     os.system("clear")
-    OPCJE=int(input("Uzytkowniku, w celu dalszego działania, wybierz opcje:\n1. Dodanie zakupu do listy\n2. Modyfikacja listy\n3. Usuwanie zakupu z listy\n4. Zakończenie\n "))
+    OPCJE=int(input("Uzytkowniku, w celu dalszego działania, wybierz opcje:\n1. Dodanie zakupu do listy\n2. Modyfikacja listy\n3. Usuwanie zakupu z listy\n4. Czyszczenie listy\n5. Zliczanie wystąpień zakupu\n6. Zakończenie\n "))
     if OPCJE==1:
         while True:
             ELEMENT_LISTY=input("Wpisz zakup:")
@@ -20,7 +21,15 @@ while True:
         USUWANIE=input("Który produkt chcesz usunąć: ")
         lista_zakupow.remove(USUWANIE)
     elif OPCJE==4:
+        lista_zakupow.clear()
+    elif OPCJE==5:
+        ZLICZANY_ELEMENT=input("Uzytkowniku, ktory zakup chcesz zliczyc?")
+        LICZENIE=lista_zakupow.count(ZLICZANY_ELEMENT)
+        print(f"Zakup {ZLICZANY_ELEMENT} wystąpił {LICZENIE} raz/y.")
+        time.sleep(5)
+    elif OPCJE==6:
         break
     else:
         print("Nie rozumiem.")
 print(lista_zakupow)
+
